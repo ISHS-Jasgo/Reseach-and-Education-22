3fc997bed4aba0da96247ff6d4ce27f02a54a877
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2022-08-09 19:41:38 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var neurosky = require('node-neurosky')
+var fs = require('fs')
+
+var client = neurosky.createClient({
+	appName:'NodeNeuroSky',
+	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+})
+
+client.on('data',function(data){
+	console.log(data)
+})
+
+client.connect()
